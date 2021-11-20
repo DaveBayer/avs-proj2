c35e543fc484e2aae024453cd1e1b54e18032f0e
DaveBayer <xbayer09@stud.fit.vutbr.cz>
2021-11-20 19:30:51 +0100

    asdf

--- parallel_builder/tree_mesh_builder.cpp
+++ parallel_builder/tree_mesh_builder.cpp
@@ -83,7 +83,7 @@ uint TreeMeshBuilder::decomposeOctree(Vec3_t<float> pos, uint size, const Parame
         for (auto sc : get_subcubes(pos, size)) {
             Vec3_t<float> S = cube_center(sc, subcube_size);
             
-            if (evaluateFieldAt(S, field) > r) {
+            if (!(evaluateFieldAt(S, field) > r)) {
 #               pragma omp task shared(totalTriangles) firstprivate(sc, subcube_size, field)
                 {
                     totalTriangles += decomposeOctree(sc, subcube_size, field);
